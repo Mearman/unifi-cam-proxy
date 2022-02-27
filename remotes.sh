f57e905a8495615014e5413b3b291a75367b1198
Joseph Mearman <joseph@mearman.co.uk>
2022-02-27 18:13:09 +0000

    script to add other remotes

---
+++ remotes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git remote -v
+
+git remote add keshavdv https://github.com/keshavdv/unifi-cam-proxy.git
+git remote add daltskin https://github.com/daltskin/unifi-cam-proxy.git
+git remote add mearman https://github.com/mearman/unifi-cam-proxy.git
+
+git remote -v
